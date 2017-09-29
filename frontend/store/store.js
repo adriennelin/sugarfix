a20b3bd0268a9ca4828365121d3832cc2d9542fb
Adrienne Lin <lin.adrienne@gmail.com>
2017-09-29 12:27:40 -0700

    update dev for production

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,14 +1,20 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import RootReducer from '../reducers/root_reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     RootReducer,
     preloadedState,
-    composeWithDevTools(applyMiddleware(thunk, logger))
+    composeWithDevTools(applyMiddleware(thunk, ...middlewares))
   )
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,20 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+
   context: __dirname,
   entry: './frontend/sugarfix.jsx',
   output: {
@@ -10,6 +24,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '*']
   },
+
   module: {
     loaders: [
       {
